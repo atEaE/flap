f87197c6c44e1dc1af7a22c929fcdfc72cd5b00d
Aoki Eta <aoki@wacul.co.jp>
2021-08-14 09:18:46 +0900

    feat: redesigned as a simple validator

---
+++ error.go
@@ -0,0 +1,42 @@
+package valigo
+
+import "fmt"
+
+// ValidateError
+type ValidateError struct {
+	name  string
+	inner error
+}
+
+// Error will output the details.
+func (e *ValidateError) Error() string {
+	return e.inner.Error()
+}
+
+// newValidateError is create error instance.
+func newValidateError(name string, err error) *ValidateError {
+	return &ValidateError{
+		name:  name,
+		inner: err,
+	}
+}
+
+// newRequiredError is create required error instance.
+func newRequiredError(name string) *ValidateError {
+	return newValidateError(name, fmt.Errorf("'%s' is required", name))
+}
+
+// newDoesNotExistsError is create doesnot exists error instance.
+func newDoesNotExistsError(name string, filepath string) *ValidateError {
+	return newValidateError(name, fmt.Errorf("'%s' no such file or directory", filepath))
+}
+
+// newNotDirError is create not directory error instance.
+func newNotDirError(name string, filepath string) *ValidateError {
+	return newValidateError(name, fmt.Errorf("'%s' is not directory", filepath))
+}
+
+// newNotFileError is create not file error instance.
+func newNotFileError(name string, filepath string) *ValidateError {
+	return newValidateError(name, fmt.Errorf("'%s' is not file", filepath))
+}
--- go.mod
+++ go.mod
@@ -1,11 +1,3 @@
-module github.com/atEaE/flap
+module github.com/atEaE/valigo
 
 go 1.13
-
-require (
-	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/objx v0.3.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	github.com/wacul/ptr v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-)
